6962c71a84bd43eb1846ffdd3706cd3b5a76b6c5
Olaide Adebanjo <olaideadebanjo@gmail.com>
2022-04-28 14:26:46 -0700

    Categories and Product Component added to the home page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,9 +5,26 @@ import { BiSearch } from "react-icons/bi";
 import { popularProducts } from "../productData";
 
 const WholeContainer = styled.div`
-  padding: 20px;
   display: flex;
   flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 3;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
 `;
 
 const Container = styled.div`
@@ -15,26 +32,57 @@ const Container = styled.div`
   margin: 5px;
   min-width: 280px;
   height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
-const Circle = styled.div``;
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  border-color: white;
+  color: white;
+  background-color: white;
+  position: absolute;
+`;
 
 const Image = styled.img`
   height: 75%;
+  z-index: 2;
 `;
 
-const Info = styled.div``;
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
 
-const Icon = styled.div``;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
 
 const Product = ({ item }) => {
   return (
     <WholeContainer>
-      <Circle />
       {popularProducts.map((item) => {
         const { id, img } = item;
         return (
           <Container key={id}>
+            <Circle />
             <Image src={img} alt="" />
             <Info>
               <Icon>
